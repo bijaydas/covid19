d589771f708033e0c55a4ff4523e47f29bf14c12
bijaydas <imbijaydas@gmail.com>
2020-06-04 00:17:30 +0530

    Added default India coordinates in World.js

--- src/components/World/World.js
+++ src/components/World/World.js
@@ -1,14 +1,21 @@
 import React, { Component, Fragment } from 'react';
 import { GoogleMap, withScriptjs, withGoogleMap } from 'react-google-maps';
-7
+
+
 import { googleMapUrl } from 'System/url';
 
 class World extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            defaultZoom: 8,
-            defaultCenter: { lat: -34.397, lng: 150.644 }
+            defaultZoom: 2,
+            
+            // Credit: www.latlong.net
+            defaultCenter: {
+                lat: 28.613939,
+                lng: 77.209023,
+                country: 'India'
+            }
         }
     }
 
@@ -17,7 +24,7 @@ class World extends Component {
         return(
             <GoogleMap
                 defaultZoom={this.state.defaultZoom}
-                defaultCenter={this.state.defaultCenter}
+                center={this.state.defaultCenter}
             >
                 
             </GoogleMap>
