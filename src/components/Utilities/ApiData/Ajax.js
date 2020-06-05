5190c2f2fa31d9503f7843fd84e43272137566fb
bijaydas <imbijaydas@gmail.com>
2020-06-06 01:24:10 +0530

    Removed ApiData from Utilities

--- src/components/Utilities/ApiData/Ajax.js
+++
@@ -1,7 +0,0 @@
-class Ajax {
-    getData(url) {
-        return fetch(url).then(_ => _.json());
-    }
-}
-
-export default Ajax;
--- src/components/Utilities/ApiData/index.js
+++
@@ -1,5 +0,0 @@
-import Covid19Ajax from './Covid19Ajax';
-
-export {
-    Covid19Ajax,
-};
