07fbafd47bf7de721b80f2355aedeb587b888286
bijaydas <imbijaydas@gmail.com>
2020-06-06 20:53:43 +0530

    Added active and inactive nav bar tab

---
+++ src/components/Header/Header.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+    faHome,
+    faGlobe
+} from '@fortawesome/free-solid-svg-icons';
+
+import 'Css/header/header.css';
+
+const Header = props => {
+    return(
+        <header className="Header full-height matt-blue">
+            <aside>
+                <div className="logo">Covid19</div>
+                <nav>
+                    <ul>
+                        <li>
+                            <Link
+                                className={props.activeTab === '' ? 'active' : null}
+                                to="/">
+                                <FontAwesomeIcon icon={faHome} />
+                            </Link>
+                        </li>
+                        <li>
+                            <Link
+                                className={props.activeTab === 'world' ? 'active' : null}
+                                to="/world">
+                                <FontAwesomeIcon icon={faGlobe} />
+                            </Link>
+                        </li>
+                    </ul>
+                </nav>
+                
+                
+            </aside>
+        </header>
+    )
+}
+
+export default Header;
