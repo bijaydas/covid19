ccb2bb892771d21a10a6db0a947d26722db0d884
bijaydas <imbijaydas@gmail.com>
2020-06-07 08:09:28 +0530

    Maded header icons smaller

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,6 +5,7 @@ import { Header } from 'Comps/Header';
 import Content from './Content';
 
 const Home = props => {
+    document.title = 'Covid19 Tracker'
     return (
         <div className="full-height grid grid-1-12">
             <Header activeTab={props.activeTab} />
