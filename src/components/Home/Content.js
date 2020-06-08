69b91f1232e42666b3acbfe97cbdc4ac2933b383
bijaydas <imbijaydas@gmail.com>
2020-06-08 23:42:34 +0530

    Made confirmed recovered and death colorful

--- src/components/Home/Content.js
+++ src/components/Home/Content.js
@@ -6,17 +6,25 @@ import {
 } from '@fortawesome/free-solid-svg-icons';
 
 import 'Css/home/home.css';
-import { getCountries } from 'Utilities';
+import { getCountries, worldTotal, formatNumber } from 'Utilities';
 
 class Content extends Component {
     constructor(props) {
         super(props);
         this.state = {
             countries: [],
+            total: {
+                confirmed: 0,
+                recovered: 0,
+                deaths: 0,
+            }
         };
         this.countryUpdate = this.countryUpdate.bind(this);
     }
     componentDidMount() {
+        /**
+         * Setting the country list in the dropdown
+         */
         getCountries((err, data) => {
             const countries = data.map((country) => {
                 return {
@@ -28,27 +36,69 @@ class Content extends Component {
                 countries: countries,
             });
         });
+        /**
+         * Selecting the world total
+         */
+        worldTotal((err, data) => {
+            const confirmed = formatNumber(data.TotalConfirmed);
+            const recovered = formatNumber(data.TotalRecovered);
+            const deaths    = formatNumber(data.TotalDeaths);
+            this.setState((state) => {
+                return {
+                    ...state,
+                    total: {
+                        confirmed,
+                        recovered,
+                        deaths,
+                    },
+                };
+            });
+        });
     }
     countryUpdate(data) {
     }
     render() {
+        const customStyles = {
+            option: (provided, state) => {
+                return {
+                    ...provided,
+                    textAlign: 'left',
+                    fontSize: '12px',
+                };
+            },
+        };
         return (
             <main className="Content content-padding">
                 <section className="left">
                     <h3>Global status</h3>
-                    <div className="status-overview-container">
-                        <div className="target">
-                            <div className="live">
-                                <FontAwesomeIcon icon={faDotCircle} />
-                                <span>live</span>
+                    <div className="grid grid-equal-4">
+                        <div className="stat-card">
+                            <div className="control">
+                                <div className="body">
+                                    <FontAwesomeIcon icon={faDotCircle} />
+                                    <span>live</span>
+                                </div>
+                            </div>
+                            <div className="footer">
+                                <Select
+                                    defaultValue={{ label: 'World', value: 'world' }}
+                                    options={this.state.countries}
+                                    onChange={this.countryUpdate}
+                                    styles={customStyles}
+                                />
                             </div>
                         </div>
-                        <div className="stats-card">
-                            <Select
-                                defaultValue={{ label: 'World', value: 'world' }}
-                                options={this.state.countries}
-                                onChange={this.countryUpdate}
-                            />
+                        <div className="stat-card card-confirmed">
+                            <div className="body">{this.state.total.confirmed}</div>
+                            <div className="footer">Confirmed</div>
+                        </div>
+                        <div className="stat-card card-recovered">
+                            <div className="body">{this.state.total.recovered}</div>
+                            <div className="footer">Recovered</div>
+                        </div>
+                        <div className="stat-card card-deaths">
+                            <div className="body">{this.state.total.deaths}</div>
+                            <div className="footer">Deaths</div>
                         </div>
                     </div>
                 </section>
