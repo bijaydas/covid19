7d59533931d0876430c6b3c022c49aec7471cce9
bijaydas <imbijaydas@gmail.com>
2020-06-09 16:22:29 +0530

    Fixed ESlint issues

--- src/utilities/CovidAjax.js
+++ src/utilities/CovidAjax.js
@@ -4,25 +4,23 @@ class CovidAjax extends Urls {
     constructor() {
         super();
     }
-    
     getCountries(callback = null) {
         fetch(this.countries())
-            .then(res => res.json())
-            .then(res => {
-                callback(null, res)
+            .then((res) => res.json())
+            .then((res) => {
+                callback(null, res);
             })
-            .catch(error => {
+            .catch((error) => {
                 callback(error, null);
             });
     }
-    
     getWorldTotal(callback = null) {
         fetch(this.worldTotal())
-            .then(res => res.json())
-            .then(res => {
-                callback(null, res)
+            .then((res) => res.json())
+            .then((res) => {
+                callback(null, res);
             })
-            .catch(error => {
+            .catch((error) => {
                 callback(error, null);
             });
     }
