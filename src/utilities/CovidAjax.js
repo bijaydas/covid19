771ca911336a04b5f9f8804c55bb30dd788eabea
bijaydas <imbijaydas@gmail.com>
2020-06-09 16:16:22 +0530

    Added CovidAjax class

---
+++ src/utilities/CovidAjax.js
@@ -0,0 +1,31 @@
+const Urls = require('./Urls');
+
+class CovidAjax extends Urls {
+    constructor() {
+        super();
+    }
+    
+    getCountries(callback = null) {
+        fetch(this.countries())
+            .then(res => res.json())
+            .then(res => {
+                callback(null, res)
+            })
+            .catch(error => {
+                callback(error, null);
+            });
+    }
+    
+    getWorldTotal(callback = null) {
+        fetch(this.worldTotal())
+            .then(res => res.json())
+            .then(res => {
+                callback(null, res)
+            })
+            .catch(error => {
+                callback(error, null);
+            });
+    }
+}
+
+module.exports = CovidAjax;
