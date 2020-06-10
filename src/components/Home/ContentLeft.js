34384dff7fcf28d9d56187aa76d4fd49f4a3c942
bijaydas <imbijaydas@gmail.com>
2020-06-10 09:37:40 +0530

    Made left and right section in separate file in Home

---
+++ src/components/Home/ContentLeft.js
@@ -0,0 +1,131 @@
+import React, { Component, Fragment } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Select from 'react-select';
+import {
+    faDotCircle,
+} from '@fortawesome/free-solid-svg-icons';
+import { Link } from 'react-router-dom';
+import {
+    formatNumber,
+    CovidAjax,
+} from 'Utilities';
+
+class ContentLeft extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            countries: [],
+            total: {
+                confirmed: 0,
+                recovered: 0,
+                deaths: 0,
+            },
+        };
+        this.countryUpdate = this.countryUpdate.bind(this);
+    }
+    componentDidMount() {
+        /**
+         * Setting the country list in the dropdown
+         */
+        const ajax = new CovidAjax();
+        ajax.getCountries((err, data) => {
+            const countries = data.map((country) => {
+                return {
+                    label: country.Country,
+                    value: country.Slug,
+                };
+            });
+            this.setState({
+                countries: countries,
+            });
+        });
+        /**
+         * Selecting the world total
+         */
+        ajax.getWorldTotal((err, data) => {
+            const confirmed = formatNumber(data.TotalConfirmed);
+            const recovered = formatNumber(data.TotalRecovered);
+            const deaths = formatNumber(data.TotalDeaths);
+            this.setState((state) => {
+                return {
+                    ...state,
+                    total: {
+                        confirmed,
+                        recovered,
+                        deaths,
+                    },
+                };
+            });
+        });
+    }
+    countryUpdate({ value }) {
+        const ajax = new CovidAjax();
+        ajax.getCountryTotalAllStatus(value, (err, data) => {
+            if (data.length > 0) {
+                const latest = data[data.length - 1];
+                this.setState((state) => {
+                    return {
+                        ...state,
+                        total: {
+                            confirmed: formatNumber(latest.Confirmed),
+                            recovered: formatNumber(latest.Recovered),
+                            deaths: formatNumber(latest.Deaths),
+                        },
+                    };
+                });
+            }
+        });
+    }
+    render() {
+        const customStyles = {
+            option: (provided, state) => {
+                return {
+                    ...provided,
+                    textAlign: 'left',
+                    fontSize: '12px',
+                };
+            },
+        };
+        return (
+                <section className="left">
+                    <h3>Global status</h3>
+                    <div className="grid grid-equal-4">
+                        <div className="stat-card">
+                            <div className="control">
+                                <div className="body">
+                                    <FontAwesomeIcon icon={faDotCircle} />
+                                    <span>live</span>
+                                </div>
+                            </div>
+                            <div className="footer">
+                                <Select
+                                    defaultValue={{ label: 'World', value: 'world' }}
+                                    options={this.state.countries}
+                                    onChange={this.countryUpdate}
+                                    styles={customStyles}
+                                />
+                            </div>
+                        </div>
+                        <div className="stat-card card-confirmed">
+                            <div className="body">{this.state.total.confirmed}</div>
+                            <div className="footer">Confirmed</div>
+                        </div>
+                        <div className="stat-card card-recovered">
+                            <div className="body">{this.state.total.recovered}</div>
+                            <div className="footer">Recovered</div>
+                        </div>
+                        <div className="stat-card card-deaths">
+                            <div className="body">{this.state.total.deaths}</div>
+                            <div className="footer">Deaths</div>
+                        </div>
+                    </div>
+                    
+                    <div className="more-info">
+                        <Link to="/anylytics">More info</Link>
+                    </div>
+                </section>
+        );
+    }
+};
+
+export default ContentLeft;
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-// import { getCountries } from 'Utilities';
 
+import 'Css/home/home.css';
 import { Header } from 'Comps/Header';
 import Content from './Content';
 
